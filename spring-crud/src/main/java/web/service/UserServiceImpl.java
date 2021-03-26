1b89ed9953744cabbfdb36817dbaaf58d7bf673d
filatovevgeny13 <76839320+filatovevgeny13@users.noreply.github.com>
2021-03-26 22:32:09 +0500

    Add files via upload

---
+++ spring-crud/src/main/java/web/service/UserServiceImpl.java
@@ -0,0 +1,40 @@
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import web.dao.UserDao;
+import web.model.User;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Override
+    public void save(User user) {
+        userDao.save(user);
+    }
+
+    @Override
+    public void delete(int id) {
+        userDao.delete(id);
+    }
+
+    @Override
+    public void update(User user) {
+        userDao.update(user);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+
+    @Override
+    public User findById(int id) {
+        return userDao.findById(id);
+    }
+}
