2856a07aaa1b61e49c5ab15f1a35d8982eb95031
filatovevgeny13 <76839320+filatovevgeny13@users.noreply.github.com>
2021-04-01 00:12:11 +0500

    Add files via upload

---
+++ spring-crud/src/main/java/web/controller/UserController.java
@@ -0,0 +1,57 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import web.model.User;
+import web.service.UserService;
+
+@Controller
+public class UserController {
+    final
+    UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping(value = "/")
+    public String printAllUsers(Model model) {
+        model.addAttribute(userService.getAllUsers());
+        return "users";
+    }
+
+    @GetMapping(value = "/addUser")
+    public String addUser(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return "userInfo";
+    }
+
+    @PostMapping("/saveUser")
+    public String saveUser(@ModelAttribute("user") User user) {
+        userService.save(user);
+        return "redirect:/";
+    }
+
+    @RequestMapping(value = "user-delete/{id}", method = {RequestMethod.DELETE, RequestMethod.GET})
+    public String deleteUser(@PathVariable("id") int id) {
+        userService.delete(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/user-update/{id}")
+    public String updateUserForm(@PathVariable("id") int id, Model model) {
+        User user = userService.findById(id);
+        model.addAttribute("user", user);
+        return "/user-update";
+    }
+
+    @PostMapping("/user-update")
+    public String updateUser(User user) {
+        userService.update(user);
+        return "redirect:/";
+    }
+}
