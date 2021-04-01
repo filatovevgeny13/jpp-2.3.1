9a5dd73f07bc1698368c6ca589fb92cf8b655b1c
filatovevgeny13 <76839320+filatovevgeny13@users.noreply.github.com>
2021-04-01 19:21:52 +0500

    Add files via upload

---
+++ spring-crud/src/main/java/web/dao/UserDao.java
@@ -0,0 +1,17 @@
+package web.dao;
+
+import web.model.User;
+
+import java.util.List;
+
+public interface UserDao {
+    void save(User user);
+
+    void delete(int id);
+
+    void update(User user);
+
+    List<User> getAllUsers();
+
+    User findById(int id);
+}
